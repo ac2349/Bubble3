583a89d8beb01d630d20971b39fb6d284c139d17
May <mayy.yang@gmail.com>
2015-03-06 10:48:47 -0800

    working on Discovery View Preferences

---
+++ speeddate/DiscoveryPreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoveryPreferencesViewController.h
+//  speeddate
+//
+//  Created by May Yang on 3/6/15.
+//  Copyright (c) 2015 Studio76. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoveryPreferencesViewController : UIViewController
+
+@end
---
+++ speeddate/DiscoveryPreferencesViewController.m
@@ -0,0 +1,37 @@
+//
+//  DiscoveryPreferencesViewController.m
+//  speeddate
+//
+//  Created by May Yang on 3/6/15.
+//  Copyright (c) 2015 Studio76. All rights reserved.
+//
+
+#import "DiscoveryPreferencesViewController.h"
+
+@interface DiscoveryPreferencesViewController ()
+
+@end
+
+@implementation DiscoveryPreferencesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
