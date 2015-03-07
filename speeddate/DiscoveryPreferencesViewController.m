73d04dfabadca4dd54478bc605df78abbaca56b6
May <mayy.yang@gmail.com>
2015-03-06 19:12:54 -0800

    working on slider

--- speeddate/DiscoveryPreferencesViewController.m
+++ speeddate/DiscoveryPreferencesViewController.m
@@ -8,11 +8,17 @@
 
 #import "DiscoveryPreferencesViewController.h"
 #import "SWRevealViewController.h"
+#import "NMRangeSlider.h"
 
 @interface DiscoveryPreferencesViewController ()
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 @property (weak, nonatomic) IBOutlet UISlider *distanceSlider;
 @property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minAge;
+@property (weak, nonatomic) IBOutlet UILabel *maxAge;
+@property (weak, nonatomic) IBOutlet NMRangeSlider *ageSlider;
+
+
 
 @end
 
@@ -28,11 +34,7 @@
         [self.sidebarButton setAction: @selector( revealToggle: )];
     }
     
-    // Distance slider code
-    self.distanceSlider.minimumValue = 1;
-    self.distanceSlider.maximumValue = 50;
-    self.distanceSlider.continuous = YES;
-    self.distanceSlider.value = 50;
+    [self configureSliders];
     
 }
 
@@ -43,6 +45,26 @@
 // #TODO: Save data.
     // 1) Remember settings within app.
     // 2) Save to phone (NSUserDefaults/Persistence).
+    
+}
+
+-(void)configureSliders
+{
+    // Distance slider code
+    self.distanceSlider.minimumValue = 1;
+    self.distanceSlider.maximumValue = 50;
+    self.distanceSlider.continuous = YES;
+    self.distanceSlider.value = 50;
+    
+    
+    // Age slider code
+    self.ageSlider.minimumValue = 18;
+    self.ageSlider.maximumValue = 100;
+
+    self.ageSlider.lowerValue = 18;
+    self.ageSlider.upperValue = 100;
+    
+    self.ageSlider.minimumRange = 1;
     
 }
 
@@ -58,7 +80,27 @@
     NSString *result = [formatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
     
     self.distanceLabel.text = [NSString stringWithFormat:@"%@mi.", result];
-    
 }
 
+
+- (IBAction)ageSliderValueChanged:(NMRangeSlider *)sender
+{
+    [self updateSliderLabels];
+}
+
+- (void)updateSliderLabels
+{
+    
+    CGPoint lowerCenter;
+    lowerCenter.x = (self.ageSlider.lowerCenter.x + self.ageSlider.frame.origin.x);
+    lowerCenter.y = (self.ageSlider.center.y - 30.0f);
+    self.minAge.center = lowerCenter;
+    self.minAge.text = [NSString stringWithFormat:@"%d", (int)self.ageSlider.lowerValue];
+    
+    CGPoint upperCenter;
+    upperCenter.x = (self.ageSlider.upperCenter.x + self.ageSlider.frame.origin.x);
+    upperCenter.y = (self.ageSlider.center.y - 30.0f);
+    self.maxAge.center = upperCenter;
+    self.maxAge.text = [NSString stringWithFormat:@"%d", (int)self.ageSlider.upperValue];
+}
 @end
